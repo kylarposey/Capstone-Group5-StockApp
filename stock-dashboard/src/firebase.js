2b2c182244ece7655b55181538a0f5c1137ac9fc
Kylar Posey <143016703+kylarposey@users.noreply.github.com>
2025-02-19 13:02:47 -0600

    Error fixes

--- stock-dashboard/src/firebase.js
+++ stock-dashboard/src/firebase.js
@@ -3,11 +3,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "group5-capstone-project.firebaseapp.com",
-  projectId: "group5-capstone-project",
-  storageBucket: "group5-capstone-project.firebasestorage.app",
-  messagingSenderId: "53778726797",
-  appId: "1:53778726797:web:9e285ea6cb93cc343d07c4"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
