86325d9da916d3b031a18c6f742906450b4470d8
Kyle Rivera <krr39@students.uwf.edu>
2025-02-28 17:14:11 -0600

    v2

--- stock-dashboard/src/App.js
+++ stock-dashboard/src/App.js
@@ -1,16 +1,12 @@
 import About from './Components/About';
-// remove the "eslint-disable-next-line" below once LoginPage is implemented
-// eslint-disable-next-line
 import LoginPage from './Components/LoginPage';
+import NewUser from './Components/newUser'; // Ensure correct import
 import './assets/css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from "./Components/Home";
 import Footer from './Components/Footer';
-
-
-
 
 // Placeholder functions
 // Replace these functions with react components in separate files
@@ -19,17 +15,19 @@ function NotFound() {
 }
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Header /> 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/newUser" element={<NewUser />} /> 
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- stock-dashboard/src/Components/LoginPage.js
+++ stock-dashboard/src/Components/LoginPage.js
@@ -1,20 +1,38 @@
-import React from 'react';
-//import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-
-//import { Navbar, Nav, Button } from 'react-bootstrap';
-//import { Link } from 'react-router-dom'; // Import Link
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../assets/css/LoginPage.css"; // Import the CSS file for styling
 
 function LoginPage() {
-   return (
-      <div>
-         Kyle Rivera
-         Login Page
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleLogin = () => {
+    // Handle login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login Page</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+        <Link to="/newUser">Create a new account</Link> {/* Link to NewUser component */}
       </div>
-      
-   );
-
+    </div>
+  );
 }
 
 export default LoginPage;
